191333cb24c70b99fc1fd21a113d833b4552271c
Tommaso Paoli <paoli7612@gmail.com>
2019-02-05 11:37:31 +0100

    create module to get query to database

---
+++ src/database.php
@@ -0,0 +1,29 @@
+<?php
+  include "classes/Utente.php";
+  include "classes/Lezione.php";
+
+  function query($sql, $classe){
+    $conn = new mysqli("localhost", "root", "", "lele");
+    $tabella = array();
+    $result = $conn->query($sql);
+    //print_r($sql);
+    if ($classe=="vuoto"){return;}
+    if ($result->num_rows == 0) {
+      return $tabella;
+    } else while($row = $result->fetch_assoc()){
+      if ($classe == "utente") $oggetto = new Utente($row);
+      elseif ($classe == "lezione") $oggetto = new Lezione($row);
+      array_push($tabella, $oggetto);
+    }
+    $conn->close();
+    return $tabella;
+  }
+
+  function getUtenti(){
+    return query("SELECT * FROM utenti;", "utente");
+  }
+
+  function getLezioni(){
+    return query("SELECT * FROM lezioni;", "lezione");
+  }
+?>
