0a74850f204eed0bf1cf81e72fa5be0f5aa9c25c
Tommaso Paoli <paoli7612@gmail.com>
2019-02-05 11:38:06 +0100

    remove test files

--- src/index.php
+++ src/index.php
@@ -30,16 +30,3 @@
   </tr>
   <?php endforeach; ?>
 </table>
-
-
-<div>
-  <form method="post">
-    <h1>Entra</h1>
-    <input type="file" name="upload" accept="application/pdf,application/vnd.ms-excel" />
-    <input type="email" name="email" placeholder="email">
-    <button type="submit"> Avanti </button>
-  </form>
-</div>
-
-<?php print_r($_POST);
-print_r(file($_POST['upload'])); ?>
