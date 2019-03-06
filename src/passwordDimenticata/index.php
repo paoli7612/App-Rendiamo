fb5b856223a49b413ec59d9838b3f8ec7e82ebfe
Tommaso Paoli <paoli7612@gmail.com>
2019-03-06 21:20:59 +0100

    Recupera password

--- src/passwordDimenticata/index.php
+++ src/passwordDimenticata/index.php
@@ -7,11 +7,16 @@
   <body>
     <?php if (isset($_GET['email'])): ?>
     <?php else: ?>
-      <div class="w3-panel w3-theme">
+      <div class="w3-panel w3-theme w3-half w3-display-topmiddle w3-card-4">
         <div class="w3-panel">
+          <h1>Recupera Password</h1>
           <form method="get">
-            <input type="text" name="email" class="w3-input w3-card-4">
-            <input type="button" name="email" class="w3-input w3-card-4">
+            <div class="w3-panel">
+              <input type="text" name="email" class="w3-input w3-card-4">
+            </div>
+            <div class="w3-panel w3-right">
+              <input type="button" name="email" class="w3-button w3-white w3-card-4" value="Conferma">
+            </div>
           </form>
         </div>
       </div>
