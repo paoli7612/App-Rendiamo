d4b2744fb520a87d8e86e157d823f66f42868eb9
Tommaso Paoli <paoli7612@gmail.com>
2019-03-09 16:30:33 +0100

    login

--- src2/accedi/index.php
+++ src2/accedi/index.php
@@ -6,7 +6,6 @@
   </head>
   <body>
     <?php session_start(); ?>
-    <?php print_r($_SESSION); ?>
     <?php include '../_database/connection.php'; ?>
     <?php include 'form.php'; ?>
     <?php include 'post.php'; ?>
--- src2/account/utente.php
+++ src2/account/utente.php
@@ -1,2 +1,4 @@
 
 <?php echo $_UTENTE->row['nome'] . " " . $_UTENTE->row['cognome'] ?>
+
+<a href="../accedi/">Disconnetti</a>
