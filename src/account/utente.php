f514bc2fe9774dfa161a790d75aa87f715a43230
Tommaso Paoli <paoli7612@gmail.com>
2019-03-17 11:45:38 +0100

    logout

--- src/account/utente.php
+++ src/account/utente.php
@@ -2,7 +2,7 @@
 <?php echo $_UTENTE->row['nome'] . " " . $_UTENTE->row['cognome'] ?>
 
 <div>
-  <a href="../accedi/">Disconnetti</a>
+  <a href="../disconnetti/">Disconnetti</a>
   <br>
   <a href="../cambiaPassword/">Cambia Password</a>
 </div>
