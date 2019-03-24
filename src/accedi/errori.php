7c1fa8e47563736a24b0aabf0a3c8d9b3b653c36
Tommaso Paoli <paoli7612@gmail.com>
2019-03-24 11:19:21 +0100

    sisi

--- src/accedi/errori.php
+++ src/accedi/errori.php
@@ -1,8 +1,8 @@
 <?php if (isset($_SESSION['wrong_email']) && $_SESSION['wrong_email']): ?>
   <?php $_SESSION['wrong_email'] = false ?>
-    <strong>Attenzione</strong> L'indirizzo email che hai inserito non corrisponde a nessun account.
+    <b>Attenzione</b> L'indirizzo email che hai inserito non corrisponde a nessun account.
 <?php endif; ?>
 <?php if (isset($_SESSION['wrong_password']) && $_SESSION['wrong_password']): ?>
   <?php $_SESSION['wrong_password'] = false ?>
-    <strong>Attenzione</strong> La passowrd non è corretta.
+    <b>Attenzione</b> La passowrd non è corretta.
 <?php endif; ?>
--- src/modificaLezione/index.php
+++ src/modificaLezione/index.php
@@ -9,7 +9,9 @@
     <?php include '../_session/start.php' ?>
     <?php include '../_head/bar.php' ?>
 
+
     <?php $lezione = getLezioneId($_GET['id'])[0]; ?>
+    <?php utentiDiLezioni($_UTENTE, $lezione) ?>
     <?php include 'options.php' ?>
 
   </body>
