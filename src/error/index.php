a3b6793690766b4c70fcea75071a7837458ff1a7
simonepaoli <35798412+simonepaoli@users.noreply.github.com>
2019-03-26 20:22:01 +0100

    Add files via upload

--- src/error/index.php
+++ src/error/index.php
@@ -11,7 +11,17 @@
       <div class="w3-panel w3-blue w3-card-4">
         <h1>Ops!</h1>
         <div class="w3-panel">
-          <p>Qualcosa è andato storto! É possibile che tu non abbia i permessi per raggiungere questa pagina</p>
+          <p><?php if ($_GET['errore']=="permesso") {
+			         echo "Qualcosa è andato storto! É possibile che tu non abbia i permessi per raggiungere questa pagina."
+				   }
+				   elseif ($_GET['errore']=="pagina") {
+			         echo "Qualcosa è andato storto! La pagina che stai cercando di raggiungere non esiste."
+				   }
+				   elseif ($_GET['errore']=="richiesta") {
+			         echo "Qualcosa è andato storto! La richiesta da te eseguita non è possibile da effettuare."
+				   }
+			?>
+		  </p>
           <button class="w3-card-3 w3-white" onclick="window.location='../home/'"> Ritorna alla Home </button>
         </div>
       </div>
