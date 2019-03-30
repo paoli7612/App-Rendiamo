61ba1ae8b0588a5288697a00df3402e7f7f05346
Tommaso Paoli <paoli7612@gmail.com>
2019-03-30 17:06:42 +0100

    Nuova lezione

--- src/nuovaLezione/form.php
+++ src/nuovaLezione/form.php
@@ -2,8 +2,8 @@
   <form method="post">
     <div class="form-group">
       <div class="form-label-group">
-        <input name="email" type="text" class="form-control" placeholder="Titolo" required="required" autofocus="autofocus">
-        <label for="inputEmail">Titolo</label>
+        <input id="titolo" name="email" type="text" class="form-control" placeholder="Titolo" required="required" autofocus="autofocus">
+        <label for="titolo">Titolo</label>
       </div>
     </div>
     <div class="form-group">
@@ -14,16 +14,43 @@
     </div>
     <div class="form-group">
       <div class="form-label-group">
-        <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#exampleModalLong">
+        <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#Materie">
           Seleziona materie
+        </button>
+        <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#Etichette">
+          Aggiungi etichette
         </button>
       </div>
     </div>
-    <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+    <div class="modal fade" id="Materie" tabindex="-1" role="dialog" aria-labelledby="MaterieTitle" aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLongTitle">Seleziona materie:</h5>
+            <h5 class="modal-title" id="MaterieTitle">Seleziona materie:</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <?php  $materie=query("SELECT * FROM materie") ?>
+            <?php foreach ($materie as $materia): ?>
+              <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" name="<?php echo $materia['id'] ?>"id="<?php echo $materia['id'] ?>">
+                <label class="custom-control-label" for="<?php echo $materia['id'] ?>"><?php echo $materia['titolo'] ?></label>
+              </div>
+            <?php endforeach; ?>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-primary" data-dismiss="modal">Conferma</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="modal fade" id="Etichette" tabindex="-1" role="dialog" aria-labelledby="EtichetteTitle" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="EtichetteTitle">Seleziona materie:</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
