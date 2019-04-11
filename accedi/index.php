023d834c2cff15e7269844b1bad8da94af8cb405
simonepaoli <35798412+simonepaoli@users.noreply.github.com>
2019-04-11 12:31:50 +0200

    Add files via upload

---
+++ accedi/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <?php include '../head.html' ?>
+    <title>Accedi</title>
+  </head>
+  <body class="bg-dark">
+    <?php include 'post.php' ?>
+    <?php include 'form.php' ?>
+  </body>
+</html>
